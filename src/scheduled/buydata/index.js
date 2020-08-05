f54d1a10cbf7e4d387e9f0224eececf7bb3ab3af
Mizzy <mohd_izzy@hotmail.com>
2020-08-05 21:31:02 +0530

    table_name_change

--- src/scheduled/buydata/index.js
+++ src/scheduled/buydata/index.js
@@ -4,9 +4,15 @@ const buyTableName = 'buyData';
 
 exports.handler = async function scheduled (event) {
   console.log(JSON.stringify(event, null, 2))
-  let table = await data.get({table: tableName})
+  let invTable = await data.get({table: tableName})
 
-  await data.set({table:buyTableName,key:table[0].key,prod1:table[0].prod1,prod2:table[0].prod2})
+  await data.set(
+    {
+    table:buyTableName,
+    key:invTable[0].key,
+    prod1:invTable[0].prod1,
+    prod2:invTable[0].prod2
+  });
   
   let bd = await data.get({table:buyTableName})
   console.log(bd)
